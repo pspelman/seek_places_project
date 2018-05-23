956b861f27e752aab7638fbfd078b613a295a1fe
Michael Konvalinka <MikeKonvalinka@gmail.com>
2018-05-22 17:33:33 -0700

    Added the Place model that will be used for adding places to be displayed on google maps

--- src/main/java/com/seeker/placer/models/Place.java
+++ src/main/java/com/seeker/placer/models/Place.java
@@ -1,5 +1,78 @@
 package com.seeker.placer.models;
 
-public class Place {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="places")
+public class Place {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    private String address;
+    
+    private float lat;
+    
+    private float lng;
+    
+    private String type;
+    
+    public Place() {
+    	
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public float getLat() {
+		return lat;
+	}
+
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+
+	public float getLng() {
+		return lng;
+	}
+
+	public void setLng(float lng) {
+		this.lng = lng;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 }
